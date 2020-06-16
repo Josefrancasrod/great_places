2aef01d46e7a0d16ab0d5812375a53e848623b85
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2020-06-15 23:27:32 -0500

    First commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/great_places.dart';
+import '../screens/add_place_screen.dart';
+import '../screens/place_detail_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Place'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: Center(child: Text('Got no places yet')),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, index) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[index].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[index].title),
+                          subtitle: Text(greatPlaces.items[index].location.address),
+                          onTap: () {
+                            Navigator.of(context).pushNamed(PlaceDetailScreen.routeName, arguments: greatPlaces.items[index].id);
+                          },
+                        ),
+                      ),
+              ),
+      ),
+    );
+  }
+}
